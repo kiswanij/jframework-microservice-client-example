98f2ec08d6714c571ef692958dbdcdf069256b68
Jalal Kiswani <kiswanij@gmail.com>
2021-03-11 10:35:06 +0200

    .

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -1,31 +1,27 @@
 package com.app;
 
-
 import java.util.List;
 
-import com.app.model.Account;
+import com.app.model.Person;
 import com.jk.util.JK;
 import com.jk.web.services.client.JKServiceClient;
 
 public class App {
 	public static void main(String[] args) {
-		JKServiceClient<Account> client=new JKServiceClient<Account>("http://localhost:8080/app/accounts", Account.class);
-		Account account=new Account();
-		account.setNumber("1234");
-		account.setName("Jalal");
-		account.setAvg(100.3);
-		account.setPhone("88888");
-		client.callJsonWithPost(account);
-		
-		// Retrieve all account
-		List<Account> list = client.callJsonAsListOfObjects();
+		JKServiceClient<Person> client = new JKServiceClient<>("http://localhost:8080/app/example", Person.class);
 
-		for (Account record: list) {
-			JK.print(record);
-		}
-		// Retrieve single account
-		account = client.callSingleJson("/1");
-		JK.printBlock(account);
-		
+		String response = client.callJsonAsString("/hello");
+		JK.print(response);
+
+		String response2 = client.callJsonAsString("/hello/Jalal");
+		JK.print(response2);
+
+		Person p = new Person();
+		p.setName("Jalal");
+		p.setAge(40);
+
+		String response3 = client.callJsonWithPost("/hello", p);
+		JK.print(response3);
+
 	}
 }
